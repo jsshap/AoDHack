fa94550142f0e3740e950dc9c6493b9549b3685f
jsshap <jsshap@gmail.com>
2021-01-01 13:28:16 -0500

    added title to event

--- Event.py
+++ Event.py
@@ -24,13 +24,13 @@ from Participant import Participant
 
 class Event:
 
-    def __init__(self, emailOfCreator: str, nameOfCreator: str, min: int, max: int, location: str, time: str, description: str):
+    def __init__(self, emailOfCreator: str, nameOfCreator: str, min: int, max: int, location: str, time: str, title: str, description: str):
         self.participants=[]
         self.participants.append(Participant(emailOfCreator,nameOfCreator))
 
         self.location= location
         self.time=time
-
+        self.title=title
         self.numPeople = 1
 
         self.min=min
