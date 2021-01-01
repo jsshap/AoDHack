c1ee019ddf77489506de14d02c6ba8a0ad7d0106
skwak22 <skwak22@amherst.edu>
2021-01-01 13:11:05 -0600

    flask form updates

--- flask-files/flasksite.py
+++ flask-files/flasksite.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import Form
+from wtforms import TextField
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-	return render_template('index.html')
+    error = ''
+    if request.method == 'POST':
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        minpart = request.form['minpart']
+        maxpart = request.form['maxpart']
+        etype = request.form['etype']
+        edesc = request.form['edesc']
+
+        if len(fname) == 0:
+            error = "Please supply a first name"
+        elif !minpart.isdigit():
+
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
