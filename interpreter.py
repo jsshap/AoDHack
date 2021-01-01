bc22115fe4b1db9c29bb140fc3296752957d0bf9
jsshap <jsshap@gmail.com>
2021-01-01 15:12:08 -0500

    updated inerpreeter

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,8 @@
 #JSON interpreter which turns JSON file into an Event object
 import json
 
-x =  '{ "Location":"Hills Field", "Time":"2:30", "Description":"Practice", "Participants":[Jordy,Sung,Jake,Vaibhav,notGeorge]}'
+x =  '{ "Location":"Hills Field", "Time":"2:30", "Description":"Practice", "Participants":[{"Name":"Jake, "Email":"jsshap@gmail.com"},{}}'
+
 def JSONtoEvent(j: json):
     parse = json.loads(j)
     print(parse)
