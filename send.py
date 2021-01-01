f89cd94461a58c7aa96e9fddd26dcba527a566fc
jsshap <jsshap@gmail.com>
2021-01-01 13:54:13 -0500

    send works now

--- send.py
+++ send.py
@@ -33,14 +33,14 @@ def sendEmail(event):
     # Email text
     email_body = '''Subject: Your Hangout Event\n
     \nHello, you have an update from Hangout!
-
-
     '''
 
-    email_body+=('\nYour event is on!\n\n' + "\nTitle: " + event.title + "\nDescription: " + event.description+ "\n\n Participants:\n")
+    email_body+=('\nYour event is on!\n\n' + "\nTitle: " + event.title + "\nDescription: " + event.description+ "\nLocation: " + event.location +"\n\nParticipants:\n")
 
     for n in names:
-        email_body+=(n+"\n")
+        email_body+=("\n"+n)
+
+    email_body += ("\n\n\nSee you there!")
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
