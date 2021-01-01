283cf13c59923b622c923a1e1ffdfe31be23b160
jsshap <jsshap@gmail.com>
2021-01-01 15:34:05 -0500

    interpreter works

--- interpreter.py
+++ interpreter.py
@@ -1,10 +1,27 @@
 #JSON interpreter which turns JSON file into an Event object
 import json
+from Event import Event
+from Participant import Participant
 
-x =  '{ "Location":"Hills Field", "Time":"2:30", "Description":"Practice", "Participants":[{"Name":"Jake", "Email":"jsshap@gmail.com"},{}]}'
+x =  '{ "min":5, "max": 10, "Location":"Hills Field", "Time":"2:30", "Description":"Practice", "Title":"Prac", "Participants":[{"Name":"Jake", "Email":"jsshap@gmail.com"}]}'
 
 def JSONtoEvent(j: json):
-    parse = json.loads(j)
-    print(parse)
-    return parse
-print(JSONtoEvent(x)["Time"])
+    event= Event(j["min"], j["max"], j["Location"], j["Time"], j["Title"], j["Description"])
+    for participant in j["Participants"]:
+        event.participants.append(Participant(participant["Name"], participant["Email"]))
+    
+    return event
+
+'''
+parse = json.loads(x)
+print(JSONtoEvent(parse))
+
+event=JSONtoEvent(parse)
+'''
+
+
+
+
+
+
+
