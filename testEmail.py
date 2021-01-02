c6a47ec39f65d3ab2006c5642975e726b0c9effb
jsshap <jsshap@gmail.com>
2021-01-02 17:49:08 -0500

    deleted old files/reorg

--- testEmail.py
+++
@@ -1,9 +0,0 @@
-from send import sendEmail
-import send
-from classes import Participant, Event
-
-testEvent= Event(0, 100, "Pratt Field", '12:00', 'Tossing with the boiz', 'Wanna Toss?')
-testEvent.participants.append(Participant('frisboysproject@gmail.com','Sung'))
-#testEvent.participants.append(Participant('test@gmail.com','Jake'))
-
-send.sendEmail(testEvent)
