c710bdb30e69563ba73dcdfb519a5ad8b8960100
jsshap <jsshap@gmail.com>
2021-01-02 11:51:00 -0500

    getting json into python

--- flask-files/flasksite.py
+++ flask-files/flasksite.py
@@ -22,5 +22,10 @@ def index():
 
     return render_template('index.html')
 
+@app.route("/", methods=['POST'])
+def get_post_json():
+    data=request.get_json()
+    print(data)
+
 if __name__ == '__main__':
     app.run(debug=True)
